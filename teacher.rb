c91cbb1014fac1bc1bd541bee9089f9edfaa3b15
demesameneshoa <demesameneshoa@gmail.com>
2023-08-29 14:48:32 +0300

    resolve linters error

--- teacher.rb
+++ teacher.rb
@@ -3,9 +3,6 @@ require_relative 'person'
 class Teacher < Person
   def initialize(name, age, specialization, permission: true)
     super(name, age, permission)
-
-
-
     @specialization = specialization
   end
 
