92c2782873870121c8646f8e46c668b3a92dbd2c
demesameneshoa <demesameneshoa@gmail.com>
2023-08-31 12:34:09 +0300

    refactor student.rb to accomodate classroom

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   def initialize(name, age, classroom, parent_permission: true)
     super(name, age, parent_permission: parent_permission)
-
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
