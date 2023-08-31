e5ed00673d551fb98c55a341dea2163e0f337011
demesameneshoa <demesameneshoa@gmail.com>
2023-08-31 23:30:51 +0300

    resolve linters error

--- app.rb
+++ app.rb
@@ -1,22 +1,12 @@
 require_relative 'person'
 
-
-
 require_relative 'student'
-
-
 
 require_relative 'teacher'
 
-
-
 require_relative 'rental'
 
-
-
 require_relative 'book'
-
-
 
 require_relative 'classroom'
 
@@ -24,110 +14,96 @@ require_relative 'classroom'
 
 # App class
 
-
-
 class App
+
   def initialize
+
     @persons = []
-
-
 
     @books = []
 
-
-
     @rentals = []
+
   end
 
+
+
   def home_page
+
     until list_of_options
-
-
 
       input = gets.chomp.to_i
 
-
-
       if input == 7
-
-
 
         puts 'Thank you!'
 
-
-
         exit
-
-
 
       end
 
-
-
       option input
 
-
-
     end
+
   end
+
+
 
   def list_all_persons
+
     puts 'People List is empty.' if @persons.empty?
 
-
-
     @persons.each do |person|
+
       puts "Name: #{person.name}, Age: #{person.age} ID: #{person.id}"
+
     end
+
   end
+
+
 
   def list_all_books
+
     puts 'Books List is empty.' if @books.empty?
 
-
-
     @books.each do |book|
+
       puts "Title: #{book.title}, Author: #{book.author}"
+
     end
+
   end
 
+
+
   def add_person
+
     print 'Do you want to create a student (1) or a teacher (2) ? [Input The Number]: '
-
-
 
     input = gets.chomp.to_i
 
-
-
     if input == 1
-
-
 
       add_student
 
-
-
     elsif input == 2
-
-
 
       add_teacher
 
-
-
     else
-
-
 
       puts 'Invalid option. Please try again'
 
-
-
     end
+
   end
 
+
+
   def add_student
+
     print 'Age : '
 
     age = gets.chomp.to_i
@@ -161,159 +137,112 @@ class App
       @persons << student
 
     end
+
   end
 
+
+
   def add_teacher
+
     puts 'Creating a teacher'
-
-
 
     print 'Age : '
 
-
-
     age = gets.chomp.to_i
-
-
 
     print 'Name : '
 
-
-
     name = gets.chomp
-
-
 
     print 'Specialization : '
 
-
-
     specialization = gets.chomp
-
-
 
     teacher = Teacher.new(name, age, specialization)
 
-
-
     @persons << teacher
+
   end
 
+
+
   def add_book
+
     puts 'Creating a book'
-
-
 
     print 'Title : '
 
-
-
     title = gets.chomp
-
-
 
     print 'Author : '
 
-
-
     author = gets.chomp
-
-
 
     book = Book.new(title, author)
 
-
-
     @books.push(book)
+
   end
 
+
+
   def add_rentals
+
     puts 'Select which book you want to rent by its Number '
-
-
 
     @books.each_with_index { |book, index| puts "Number: #{index + 1}. Title: #{book.title}, Author: #{book.author}" }
 
-
-
     book_id = gets.chomp.to_i - 1
-
-
 
     puts 'Select which person will rent the book by its Number '
 
-
-
-    @persons.each_with_index { |person, index| puts "#{index + 1}. Name: #{person.name}, Age: #{person.age}" }
-
-
+    @persons.each_with_index { |person, index| puts "Number: #{index + 1}. Name: #{person.name}, Age: #{person.age}" }
 
     person_id = gets.chomp.to_i - 1
 
-
-
-    print 'Date : '
-
-
+    print 'Date (YYYY/MM/DD): '
 
     date = gets.chomp.to_s
 
-
-
     rental = Rental.new(date, @books[book_id], @persons[person_id])
-
-
 
     @rentals << rental
 
-
-
     puts 'Rental created successfully'
+
   end
 
+
+
   def list_all_rentals
+
     puts 'List is empty. Please add a rental' if @rentals.empty?
-
-
 
     puts 'Enter the ID of the rental you want to see'
 
-
-
     @persons.each { |person| puts "ID: #{person.id}, Name: #{person.name}" }
-
-
 
     id = gets.chomp.to_i
 
-
-
     puts 'Rented books:'
 
-
-
     @rentals.each do |rental|
+
       if rental.person.id == id
-
-
 
         print "Person: #{rental.person.name} Date: #{rental.date} "
 
-
-
         puts "Book: #{rental.book.title}, Author: #{rental.book.author}"
-
-
 
       else
 
-
-
         puts 'No rentals found'
 
-
-
       end
+
     end
+
   end
+
 end
+
