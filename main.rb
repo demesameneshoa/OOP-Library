3773364a35de0d6d8316fa616490a9113de29358
demesameneshoa <demesameneshoa@gmail.com>
2023-08-31 12:59:03 +0300

    refactor student and teacher

--- main.rb
+++ main.rb
@@ -1,24 +1,25 @@
 require_relative 'person'
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'book'
-require_relative 'rental'
-require_relative 'classroom'
 
+require_relative 'student'
+
+require_relative 'teacher'
+
+require_relative 'book'
+
+require_relative 'rental'
+
+require 'date'
+require_relative 'classroom'
 # create a new classroom
 math_classroom = Classroom.new('Math')
 # create new student
 student1 = Student.new('James', 15, math_classroom)
-
 # create a new book
 book1 = Book.new('1984', 'George Orwell')
-
 # create a person (teacher)
-teacher1 = Teacher.new('Wilson', 45, 'Science')
-
+teacher1 = Teacher.new('Wilson', 45, 'Science', parent_permission: true)
 # create a rental
 rental1 = Rental.new(Date.today, book1, teacher1)
-
 puts student1.classroom.label # Output: Math
 puts math_classroom.students.first.name # Output: James
 puts rental1.book.title # Output: 1984
--- student.rb
+++ student.rb
@@ -1,10 +1,17 @@
 require_relative 'person'
 # Class representing a student
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(name, age, classroom, parent_permission: true)
     super(name, age, parent_permission: parent_permission)
     @classroom = classroom
     classroom.add_student(self)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
